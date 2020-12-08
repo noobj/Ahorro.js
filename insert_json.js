e5ed9bb8df17f206cfcddb132d10b7ea1db94376
jjj <jingfu99@gmail.com>
2020-12-08 09:33:32 +0800

    [Backend] Fetch json from google drive

--- insert_json.js
+++ insert_json.js
@@ -8,40 +8,47 @@ const rawdata = fs.readFileSync('ahorro.json');
 
 let objs = JSON.parse(rawdata);
 
+const readline = require('readline');
+const { google } = require('googleapis');
+
 const url = 'mongodb://localhost:27017';
 const dbName = 'ahorro';
 const client = new MongoClient(url, {});
-client.connect()
-    .then(async (connectedClient) => {
-        console.log('mongodb is connected');
-        const db = client.db(dbName);
-        let countInserted = 0;
-        const worker = async function (data) {
-                const collection = db.collection('entries');
-                await Promise.all(objs.tables[0].items.map((v) => {
-                    v._id = parseInt(v._id);
-                    v.amount = parseInt(v.amount);
-                    return collection.insertOne(v)
-                        .then(() => {
-                            countInserted++;
-                            console.log(countInserted);
-                        })
-                        .catch(err => {});
-                }));
-                return countInserted;
-        };
 
-        // 回應
-        await worker(objs.tables[0])
-            console.log('Inserted ' + countInserted + ' items.');
-        // .catch(error => {
-        //     console.error(error);
-        // });
-    })
+insertEntries();
+
+async function insertEntries() {
+    await client.connect();
+    console.log('mongodb is connected');
+    const db = client.db(dbName);
+    let countInserted = 0;
+    const worker = async function (data) {
+        const collection = db.collection('entries');
+        await Promise.all(objs.tables[0].items.map((v) => {
+            v._id = parseInt(v._id);
+            v.amount = parseInt(v.amount);
+            return collection.updateOne({ "_id" : v._id },
+            { $set: v }, {upsert: true})
+                .then(() => {
+                    countInserted++;
+                    console.log(countInserted);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }));
+        return countInserted;
+    };
+
+    await worker(objs.tables[0])
     .then((a) => {
         client.close();
     })
     .catch(error => {
         console.error(error);
     });
+
+    console.log('Inserted ' + countInserted + ' items.');
+}
+
 
